cc4fa29613cf17bcb51479c165d19eb5cdf628e5
Pony <oponymail@gmail.com>
2021-12-10 11:09:26 +0800

    Create RemoteTaskControlTest.cs

---
+++ RemoteTaskControlTest.cs
@@ -0,0 +1,30 @@
+class RemoteTaskControlTest{
+  public void TestRemoteTask()
+  {
+      string server = "terasrv061";
+      string username = "cimadmin";
+      string domain = "terapower";
+      string password = "ciMaDmin01";
+      // windows 2008 , library use ver 2, flag = false
+      //using (var ts = new TaskService(server, username, domain, password, false))
+      //{
+      //    //return ts.GetRunningTasks(true).FirstOrDefault(t => t.Name == "taskName") != null;
+      //    Debug.WriteLine(ts);
+      //    Console.WriteLine("");
+      //}
+
+      // windows 2003 , library use ver 1, flag = true
+      var ts = new TaskService(server, username, domain, password, true);
+      foreach (Task task in ts.GetFolder("Microsoft").Tasks)
+      {
+          if (task.Name == "pony_test")
+          {
+              //task.Stop();
+              task.Run();
+          }
+          Debug.WriteLine(task.Name);
+          Console.WriteLine("");
+          //do things
+      }
+  }
+}
